e42f40bd76f71f1b1a6115e7a300c21e9626b00c
kazuumi <kazuumi@user-no-MacBook-Pro.local>
2017-06-25 01:59:02 +0900

    詳細画面を追加

--- src/actions/EventAction.js
+++ src/actions/EventAction.js
@@ -1,7 +1,8 @@
 import { types } from '../consts/ActionTypes';
 
 export const openEvent = (navigation, event) => {
-	navigation.navigate('EventScreen');
+	console.log('event is :', event)
+	navigation.navigate('EventScreen', {event: event});
 	
 	return {
 		type: types.ACTION_OPEN_EVENT,
--- src/screens/EventScreen.js
+++ src/screens/EventScreen.js
@@ -1,15 +1,62 @@
 import React, { Component } from 'react';
 import {
+	ActivityIndicator,
+	WebView,
 	View,
-	Text,
 } from 'react-native';
 
+const styles = {
+	container: {
+		flex: 1,
+	},
+	indicator: {
+		position: 'absolute',
+		top: 0,
+		left: 0,
+		right: 0,
+		bottom: 0,
+	},
+	webview: {
+		flex: 1,
+	},
+}
+
 export default class EventScreen extends Component {
+	constructor(props) {
+		super(props);
+
+		const params = props.navigation.state.params;
+		this.state = {
+			event: params.event,
+			isLoading: true,
+		};
+	}
+
+	onLoadEnd() {
+		this.setState({
+			isLoading: false,
+		});
+	}
+
 	render() {
+		const {
+			event,
+			isLoading,
+		} = this.state;
+
 		return (
-			<View>
-				<Text>イベント詳細</Text>
+			<View style={styles.container}>
+				<WebView
+					
+					source={{ uri: event.item.event_url }}
+					onLoadEnd={this.onLoadEnd.bind(this)}
+				/>
+				{isLoading ?
+					<ActivityIndicator
+						style={styles.indicator}
+						animating
+					/> : null}
 			</View>
-		);
+		)
 	}
 }
